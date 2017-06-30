a41579717d41e9f1338d87f64bed53d4352f30a9
Jacob Dowdy <jacobdowdy@rocketmail.com>
2017-06-29 23:32:00 -0400

    Update .py

--- .py
+++ .py
@@ -6,7 +6,7 @@ lloyd = {
     "tests": [75.0, 90.0]
 }
 alice = {
-    "name": "Alices",
+    "name": "Alice",
     "homework": [101.0, 92.0, 98.0, 100.0],
     "quizzes": [82.0, 83.0, 91.0],
     "tests": [89.0, 97.0]
